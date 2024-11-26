264c707dfd2e1d0325ce930f0db6bbc35b641d12
pranavpm12 <pranavpm12@gmail.com>
2024-11-26 15:23:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const { default: mongoose } = require('mongoose')
 
 const app = express()
 app.use(express.json())
+
 app.use(express.urlencoded({extended:true}))
 app.get('/', (req,res)=>{ 
 res.send('home page')
